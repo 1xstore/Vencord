69ed703bd942afd8553bf0f1af0a40fc8e972883
1xstore <ryankharj@gmail.com>
2026-03-07 04:59:52 +0300

    Update index.ts

--- src/plugins/rzReply/index.ts
+++ src/plugins/rzReply/index.ts
@@ -33,10 +33,8 @@ const createMessageRecord = findByCodeLazy(".createFromServer(", ".isBlockedForM
 export default definePlugin({
     name: "ValidReply",
     description: 'Fixes "Message could not be loaded" upon hovering over the reply',
-    authors:  [{
-        name: "rz30",
-        id: 786315593963536415n
-    }, {
+    authors:  [Devs.r3r1, Devs.rz30
+    , {
         name: "l2cu",
         id: 1208352443512004648n
 }],
