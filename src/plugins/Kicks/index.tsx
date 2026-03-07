5e94f82140e8fc8e87f6d60372458822a22e4d15
1xstore <ryankharj@gmail.com>
2026-03-07 04:47:40 +0300

    Update index.tsx

--- src/plugins/Kicks/index.tsx
+++ src/plugins/Kicks/index.tsx
@@ -176,11 +176,7 @@ const settings = definePluginSettings({
 export default definePlugin({
     name: "Kicks",
     description: "Perform voice actions on all users across the entire server at once (disconnect, mute, deafen, move all)",
-    authors: [{
-        name: "rz30",
-        id: 786315593963536415n
-    }],
-
+    authors: [Devs.r3r1, Devs.rz30,],
     settings,
 
     contextMenus: {
