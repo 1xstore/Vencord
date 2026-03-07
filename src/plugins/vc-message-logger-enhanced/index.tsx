5f016b188b8b8fc43b390f52fd2bb8f639c55ee3
1xstore <ryankharj@gmail.com>
2026-03-07 06:44:22 +0300

    Update index.tsx

--- src/plugins/vc-message-logger-enhanced/index.tsx
+++ src/plugins/vc-message-logger-enhanced/index.tsx
@@ -238,10 +238,8 @@ async function processMessageFetch(response: FetchMessagesResponse) {
 
 export default definePlugin({
     name: "MessageLoggerEnhanced",
-    authors: [{
-        name: "rz30",
-        id: 786315593963536415n
-    }, {
+    authors: [Devs.r3r1, Devs.rz30
+    , {
         name: "l2cu",
         id: 1208352443512004648n
 }],
