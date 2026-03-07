2bba81224e658dd4ee91039a1fba06d62734e6bc
1xstore <ryankharj@gmail.com>
2026-03-07 06:29:59 +0300

    Update index.tsx

--- src/plugins/translate/index.tsx
+++ src/plugins/translate/index.tsx
@@ -64,10 +64,8 @@ let tooltipTimeout: any;
 export default definePlugin({
     name: "مترجم قوقل",
     description: "يترجم رسالتك و الرساله الي تبي يترجمها لك",
-    authors: [{
-        name: "rz30",
-        id: 786315593963536415n
-    }, {
+    authors: [Devs.r3r1, Devs.rz30
+    , {
         name: "l2cu",
         id: 1208352443512004648n
 }],
