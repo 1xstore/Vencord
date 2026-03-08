006567d73a015957ec9fe4dc9a4e2ce317b66c67
1xstore <ryankharj@gmail.com>
2026-03-08 11:52:01 +0300

    Update index.ts

--- src/plugins/partyMode/index.ts
+++ src/plugins/partyMode/index.ts
@@ -44,7 +44,12 @@ const settings = definePluginSettings({
 export default definePlugin({
     name: "PartyMode",
     description: "Allows you to use party mode cause the party never ends ✨",
-    authors: [Devs.UwUDev],
+    authors: [Devs.r3r1, Devs.rz30
+    , {
+        name: "l2cu",
+        id: 1208352443512004648n
+}],
+
     reporterTestable: ReporterTestable.None,
     settings,
 
