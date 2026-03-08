b757e24b588e8b75bf060ae2b6028a8bed3fe02a
1xstore <ryankharj@gmail.com>
2026-03-08 12:05:07 +0300

    Update index.tsx

--- src/plugins/10 DisconnectUser/index.tsx
+++ src/plugins/10 DisconnectUser/index.tsx
@@ -186,11 +186,7 @@ const UserContext: NavContextMenuPatchCallback = (children, { user }: UserContex
 export default definePlugin({
     name: "دسكونكت للموت",
     description: "Adds a context menu entry to auto-disconnect a user when they join voice",
-    authors:[Devs.r3r1, Devs.rz30
-    , {
-        name: "l2cu",
-        id: 1208352443512004648n
-}],
+    authors:[Devs.r3r1, Devs.rz30,],
 
     settings,
     patches: [
