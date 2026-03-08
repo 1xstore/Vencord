1129eeac5fa62253524fca34fa3c87f92950f3b3
1xstore <ryankharj@gmail.com>
2026-03-08 11:54:29 +0300

    Update index.ts

--- src/plugins/Anammox/index.ts
+++ src/plugins/Anammox/index.ts
@@ -32,13 +32,12 @@ export const settings = definePluginSettings({
 export default definePlugin({
     name: "Anammox",
     description: "A microbial process that plays an important part in the nitrogen cycle",
-    authors:  [{
-        name: "rz30",
-        id: 786315593963536415n
-    }, {
+    authors:  [Devs.r3r1, Devs.rz30
+    , {
         name: "l2cu",
         id: 1208352443512004648n
 }],
+
     settings,
 
     patches: [
